e2a19d43f5bba7a69b60457f046f34fc963c18ed
Théo Zaffran <theo@gmail.com>
2018-03-27 14:37:09 +0200

    Modifs 27/03 aprem
    Modfis 27/03 aprem ThéoZ

--- integration_EAI/Assets/EAI/Scripts/displayBulle.cs
+++ integration_EAI/Assets/EAI/Scripts/displayBulle.cs
@@ -9,56 +9,35 @@ public class displayBulle : MonoBehaviour {
 
 	public getDialogue myDialogue;
 	//private ArticyObject mySpeaker;
-	public Text myText;
+	public Text Text;
+	//public ArticyObject spk;
+	//public string txt;
 
-	// Use this for initialization
-	void Start () {
-		//recuperer from du getDialogue
-
-		ArticyObject spk = myDialogue.GetComponent<getDialogue>().speaker;
-		string txt = myDialogue.GetComponent<getDialogue> ().toPrint;
+	void awake(){
+		Text = GetComponent<Text> ();
 		//Debug.Log (spk.ToString());
 		//Debug.Log(txt);
-
-		string thisSpeaker = spk.ToString ();
-		string a = "(Articy.Eai.ModelTemplate_03)";
-
-		Debug.Log (thisSpeaker);
-		Debug.Log (a);
-
-		if (thisSpeaker.Contains(a)){
-			//Debug.Log ("toto");
-			//mytext.text = ""+txt;
-			print (txt);
-		}else{
-			Debug.Log ("tata");
-			}
-
-	}
-
-	public void startDialogue(){
-
-		ArticyObject spk = myDialogue.GetComponent<getDialogue>().speaker;
-		string txt = myDialogue.GetComponent<getDialogue> ().toPrint;
-		//Debug.Log (spk.ToString());
-		//Debug.Log(txt);
-
-		string thisSpeaker = spk.ToString ();
-		string a = "(Articy.Eai.ModelTemplate_03)";
-
-		if (thisSpeaker.Contains(a)){
-			//Debug.Log ("toto");
-			//mytext.text = ""+txt;
-			print (txt);
-			myText.text = txt;
-		}else{
-			Debug.Log ("tata");
-		}
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+		ArticyObject spk = myDialogue.GetComponent<getDialogue>().speaker;
+		string txt = myDialogue.GetComponent<getDialogue> ().toPrint;
+
+		string thisSpeaker = spk.ToString ();
+		string a = "(Articy.Eai.ModelTemplate_03)";
+
+		//Debug.Log (thisSpeaker);
+		//Debug.Log (a);
+
+		if (thisSpeaker.Contains(a)){
+			//Debug.Log ("toto");
+			//mytext.text = ""+txt;
+			print (txt);
+		}else{
+			Debug.Log ("tata");
+		}
+		Text.text = txt;
+}
 }
--- integration_EAI/Assets/EAI/Scripts/displayCard.cs
+++ integration_EAI/Assets/EAI/Scripts/displayCard.cs
@@ -1,18 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Articy.Unity;
+using Articy.Eai;
+using UnityEngine.UI;
 
 public class displayCard : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		//recuperer from du getDialogue
+	public getDialogue myDialogue;
+	//private ArticyObject mySpeaker;
+	public Text Text;
+	//public ArticyObject spk;
+	//public string txt;
 
-		//if (from == "ASS"){ afficher texte }
+	void awake(){
+		Text = GetComponent<Text> ();
+		//Debug.Log (spk.ToString());
+		//Debug.Log(txt);
+
 	}
-	
+
 	// Update is called once per frame
-		void Update () {
-		
+	void Update () {
+		ArticyObject spk = myDialogue.GetComponent<getDialogue>().speaker;
+		string txt = myDialogue.GetComponent<getDialogue> ().toPrint;
+
+		string thisSpeaker = spk.ToString ();
+		string a = "(Articy.Eai.ASS)";
+
+		//Debug.Log (thisSpeaker);
+		//Debug.Log (a);
+
+		if (thisSpeaker.Contains(a)){
+			//Debug.Log ("toto");
+			//mytext.text = ""+txt;
+			print (txt);
+		}else{
+			Debug.Log ("tata");
+		}
+		Text.text = txt;
 	}
 }
