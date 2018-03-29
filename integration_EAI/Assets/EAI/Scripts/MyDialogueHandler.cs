d68cba47c1533b04c839aac46b36a6ddd461a0be
Théo Zaffran <theo.zaffran@gmail.com>
2018-03-29 14:32:31 +0200

    29/03
    29/03
    need organiser scene avec prefabs, etc pour display les dialogues dans les bulles / cartes

--- integration_EAI/Assets/EAI/Scripts/MyDialogueHandler.cs
+++ integration_EAI/Assets/EAI/Scripts/MyDialogueHandler.cs
@@ -2,11 +2,18 @@
 using Articy.Unity.Interfaces;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class MyDialogueHandler : MonoBehaviour, IArticyFlowPlayerCallbacks
 {
 	// every pause we remember the very first branch, so we can continue the flow using it
 	private Branch firstBranch;
+
+	public bool showFalseBranches = false;
+	public GameObject branchPrefab;
+	public RectTransform branchLayoutPanel;
+	private ArticyFlowPlayer flowPlayer;
 
 	public void OnFlowPlayerPaused(IFlowObject aObject)
 	{
@@ -27,6 +34,34 @@ public class MyDialogueHandler : MonoBehaviour, IArticyFlowPlayerCallbacks
 		// just store the first branch on every pause
 		if (aBranches.Count > 0)
 			firstBranch = aBranches[0];
+
+
+
+	// we clear all old branch buttons
+		ClearAllBranches();
+
+		// for every branch provided by the flow player, we will create a button in our vertical list
+		foreach (var branch in aBranches)
+		{
+			// if the branch is invalid, because a script evaluated to false, we don't create a button, unless we want to see false branches.
+			if (!branch.IsValid && !showFalseBranches) continue;
+
+			// we create a our button prefab and parent it to our vertical list
+			var btn = Instantiate(branchPrefab);
+			var rect = btn.GetComponent<RectTransform>();
+			rect.SetParent(branchLayoutPanel, false);
+
+			// here we make sure to get the Branch component from our button, either by referencing an already existing one, or by adding it.
+			var branchBtn = btn.GetComponent<ArticyDebugBranch>();
+			if(branchBtn == null)
+				branchBtn = btn.AddComponent<ArticyDebugBranch>();
+
+			// this will assign the flowplayer and branch and will create a proper label for the button.
+			branchBtn.AssignBranch(flowPlayer, branch);
+		}
+
+
+
 	}
 
 	// Update is called once per frame
@@ -42,5 +77,14 @@ public class MyDialogueHandler : MonoBehaviour, IArticyFlowPlayerCallbacks
 			if (firstBranch != null)
 				GetComponent<ArticyFlowPlayer>().Play(firstBranch);
 		}
+
 	}
+
+
+	private void ClearAllBranches()
+	{
+		foreach (Transform child in branchLayoutPanel)
+			Destroy(child.gameObject);
+	}
+
 }
